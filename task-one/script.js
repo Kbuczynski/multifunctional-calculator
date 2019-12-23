d717c66d61b3048758296978f2ff449837b99bce
Kbuczynski <kamil.buczynski00@interia.pl>
2019-12-23 22:38:23 +0100

    change idea

--- task-one/script.js
+++ task-one/script.js
@@ -1,67 +1,55 @@
 const view = document.querySelector("#view");
-const buttons = document.querySelectorAll("button");
-const arr = [];
-
-const calc = arr => {
-  let number = "";
-  const numbers = [];
-  let result = 0;
-  let i = 0;
-
-  arr.forEach(element => {
-    if (!isNaN(element) || element == ".") {
-      number += element.toString();
-      numbers[i] = number;
-    } else {
-      number = "";
-      i++;
-      console.log(i);
-
-      if (numbers.length > 1) {
-        switch (element) {
-          case "+":
-            break;
-          case "-":
-            break;
-          case "*":
-            break;
-          case "/":
-            break;
-          default:
-            break;
-        }
-      }
-    }
-  });
-
-  console.log(numbers);
-  console.log(`Result: ${result}`);
-};
+const message = document.querySelector("#message");
+const buttons = document.querySelectorAll("input[type=button]");
+const signs = ["+", "-", "*", "/"];
+let equation = "";
+let lastNumber = "";
+const errorMessage = "Something went wrong\n press C to continue";
+let i = 0;
 
 buttons.forEach(element => {
   element.addEventListener("click", e => {
-    const sign = e.target.innerText;
+    const sign = e.target.value;
 
-    if (sign.toString().length < 3) {
-      if (!isNaN(sign)) arr.push(parseFloat(e.target.innerText));
-      else {
-        switch (sign) {
-          case "C":
-            arr.splice(0, arr.length);
-            break;
-          case "CE":
-            arr.pop();
-            break;
-          case "=":
-            calc(arr);
-            break;
-          default:
-            arr.push(sign);
-            break;
+    switch (sign) {
+      case "C":
+        equation = "";
+        message.innerText = "";
+        break;
+      case "CE":
+        equation = equation.toString().substring(0, equation.toString().length - 1);
+        break;
+      case "=":
+        try {
+          if (eval(equation).toString() == "Infinity") {
+            equation = "";
+            message.innerText = errorMessage;
+          } else equation = eval(equation);
+        } catch {
+          message.innerText = errorMessage;
         }
-      }
-    }
+        break;
+      default:
+        signs.forEach(element => {
+          if (sign == element) i++;
+        });
 
-    view.value = arr.toString().replace(/,/g, "");
+        if (i > 1) {
+          i = 1;
+          equation = eval(equation);
+          equation += sign;
+        } else equation += sign;
+        break;
+    }      
+
+    if (isNaN(equation) && equation != ".") {
+      signs.forEach(element => {
+        if (equation.split(element)[1] != null) {
+          if (equation.split(element)[1] == "") lastNumber = equation.split(element)[0];
+          else lastNumber = equation.split(element)[1];
+        }
+      });
+      view.value = lastNumber;
+    } else view.value = equation;
   });
 });
